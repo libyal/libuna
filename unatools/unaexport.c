b4a7cb76c6f2cce4d284d21875556603333403bc
Joachim Metz <joachim.metz@gmail.com>
2009-08-20 20:38:02 +0200

    Changes of version: 20090819

--- unatools/unaexport.c
+++ unatools/unaexport.c
@@ -934,7 +934,7 @@ ssize64_t unaexport(
 
 /* The main program
  */
-#if defined( HAVE_WIDE_SYSTEM_CHARACTER_T )
+#if defined( LIBSYSTEM_HAVE_WIDE_CHARACTER )
 int wmain( int argc, wchar_t * const argv[] )
 #else
 int main( int argc, char * const argv[] )
