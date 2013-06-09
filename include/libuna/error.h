173032b37e24a024942fb481e3066db2284ebd34
Joachim Metz <joachim.metz@gmail.com>
2013-06-09 20:15:07 +0200

    Code clean up

--- include/libuna/error.h
+++ include/libuna/error.h
@@ -87,9 +87,6 @@ enum LIBUNA_ARGUMENT_ERROR
 	LIBUNA_ARGUMENT_ERROR_CONFLICTING_VALUE		= 9
 };
 
-/* TODO deprecated remove after a while */
-#define LIBUNA_ARGUMENT_ERROR_VALUE_OUT_OF_RANGE	LIBUNA_ARGUMENT_ERROR_VALUE_OUT_OF_BOUNDS
-
 /* The conversion error codes
  * to signify errors regarding conversions
  */
@@ -121,9 +118,6 @@ enum LIBUNA_COMPRESSION_ERROR
 	 */
 	LIBUNA_COMPRESSION_ERROR_DECOMPRESS_FAILED	= 2
 };
-
-/* TODO deprecated remove after a while */
-#define LIBERROR_COMPRESSION_ERROR_UNCOMPRESS_FAILED	LIBERROR_COMPRESSION_ERROR_DECOMPRESS_FAILED
 
 /* The input/output error codes
  * to signify errors regarding input/output
@@ -193,9 +187,6 @@ enum LIBUNA_INPUT_ERROR
 	 */
 	LIBUNA_INPUT_ERROR_VALUE_MISMATCH		= 4
 };
-
-/* TODO deprecated remove after a while */
-#define LIBUNA_INPUT_ERROR_CRC_MISMATCH			LIBUNA_INPUT_ERROR_CHECKSUM_MISMATCH
 
 /* The memory error codes
  * to signify errors regarding memory
@@ -284,9 +275,6 @@ enum LIBUNA_RUNTIME_ERROR
 	 */
 	LIBUNA_RUNTIME_ERROR_ABORT_REQUESTED		= 15
 };
-
-/* TODO deprecated remove after a while */
-#define LIBUNA_RUNTIME_ERROR_VALUE_OUT_OF_RANGE	LIBUNA_RUNTIME_ERROR_VALUE_OUT_OF_BOUNDS
 
 /* The output error codes
  */
