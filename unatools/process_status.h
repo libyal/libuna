c8fcf4ba1f9dcfffa192701bba33ffbbdcba04f4
Joachim Metz <joachim.metz@gmail.com>
2026-05-05 14:34:56 +0200

    Applied updates

--- unatools/process_status.h
+++ unatools/process_status.h
@@ -1,7 +1,7 @@
 /*
  * Process status functions
  *
- * Copyright (c) 2008-2012, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2026, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
