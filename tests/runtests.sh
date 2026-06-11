0c025fded21e954c889f2da6a59be9295d831e8c
Joachim Metz <joachim.metz@gmail.com>
2026-06-11 21:07:39 +0200

    Replaced test scripts with autotest

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20201121
+# Version: 20260609
 
-if test -f ${PWD}/libuna/.libs/libuna.1.dylib && test -f ./pyuna/.libs/pyuna.so;
+if test -f ${PWD}/libuna/.libs/libuna.1.dylib && test -f ./pyuna/.libs/pyuna.so
 then
-	install_name_tool -change /usr/local/lib/libuna.1.dylib ${PWD}/libuna/.libs/libuna.1.dylib ./pyuna/.libs/pyuna.so;
+	install_name_tool -change /usr/local/lib/libuna.1.dylib ${PWD}/libuna/.libs/libuna.1.dylib ./pyuna/.libs/pyuna.so
 fi
 
-make check CHECK_WITH_STDERR=1;
-RESULT=$?;
+make check-build > /dev/null
 
-if test ${RESULT} -ne 0 && test -f tests/test-suite.log;
+make check $@
+RESULT=$?
+
+if test ${RESULT} -ne 0
 then
-	cat tests/test-suite.log;
+	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
-exit ${RESULT};
+exit ${RESULT}
 
