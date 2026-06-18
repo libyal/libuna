ce58e2bb612cae84f9d88fe376d09d61c6157a4b
Joachim Metz <joachim.metz@gmail.com>
2026-06-18 05:29:43 +0200

    Worked on tests and changes for CI tests

--- tests/test_library.ps1
+++ tests/test_library.ps1
@@ -1,243 +1,16 @@
 # Tests library functions and types.
-#
-# Version: 20260608
-
-$ExitSuccess = 0
-$ExitFailure = 1
-$ExitIgnore = 77
 
 $LibraryTests = "base16_stream base32_stream base64_stream byte_stream codepage_koi8_r codepage_koi8_u codepage_mac_arabic codepage_mac_celtic codepage_mac_centraleurroman codepage_mac_croatian codepage_mac_cyrillic codepage_mac_dingbats codepage_mac_farsi codepage_mac_gaelic codepage_mac_greek codepage_mac_icelandic codepage_mac_inuit codepage_mac_roman codepage_mac_romanian codepage_mac_russian codepage_mac_symbol codepage_mac_thai codepage_mac_turkish codepage_mac_ukrainian codepage_windows_874 codepage_windows_932 codepage_windows_936 codepage_windows_949 codepage_windows_950 codepage_windows_1250 codepage_windows_1251 codepage_windows_1252 codepage_windows_1253 codepage_windows_1254 codepage_windows_1255 codepage_windows_1256 codepage_windows_1257 codepage_windows_1258 error support unicode_character url_stream utf16_stream utf16_string utf32_stream utf32_string utf7_stream utf8_stream utf8_string"
 $LibraryTestsWithInput = ""
-$OptionSets = ""
+$OptionSets = "" -split " "
 
-$InputGlob = "*"
-
-$VSDirectories = @(
-	"msvscpp",
-	"vs2008",
-	"vs2010",
-	"vs2012",
-	"vs2013",
-	"vs2015",
-	"vs2017",
-	"vs2019",
-	"vs2022",
-	"vs2026"
-)
-
-$VSConfigurations = @(
-	"Release",
-	"VSDebug"
-)
-
-$VSPlatforms = @(
-	"Win32",
-	"x64"
-)
-
-Function GetTestExecutablesDirectory
-{
-	$TestExecutablesDirectory = ""
-
-	ForEach (${VSDirectory} in $VSDirectories)
-	{
-		ForEach (${VSConfiguration} in $VSConfigurations)
-		{
-			ForEach (${VSPlatform} in $VSPlatforms)
-			{
-				$TestExecutablesDirectory = "..\${VSDirectory}\${VSConfiguration}\${VSPlatform}"
-
-				If (Test-Path ${TestExecutablesDirectory})
-				{
-					Return ${TestExecutablesDirectory}
-				}
-			}
-			$TestExecutablesDirectory = "..\${VSDirectory}\${VSConfiguration}"
-
-			If (Test-Path ${TestExecutablesDirectory})
-			{
-				Return ${TestExecutablesDirectory}
-			}
-		}
-	}
-	Return ${TestExecutablesDirectory}
-}
-
-Function ReadIgnoreList
-{
-	param( [string]$TestProfileDirectory )
-
-	$IgnoreFile = "${TestProfileDirectory}\ignore"
-	$IgnoreList = ""
-
-	If (Test-Path -Path ${IgnoreFile} -PathType Leaf)
-	{
-		$IgnoreList = Get-Content -Path ${IgnoreFile} |
-			Where {$_ -notmatch '^#.*'}
-	}
-	Return $IgnoreList
-}
-
-Function RunTest
-{
-	param( [string]$TestType )
-
-	$TestDescription = "Testing: ${TestName}"
-	$TestExecutable = "${TestExecutablesDirectory}\una_test_${TestName}.exe"
-
-	If (-Not (Test-Path -Path ${TestExecutable} -PathType Leaf))
-	{
-		Write-Host "${TestDescription} (" -nonewline
-		Write-Host "SKIP" -foreground Cyan -nonewline
-		Write-Host ")"
-
-		Return ${ExitIgnore}
-	}
-	$Output = Invoke-Expression ${TestExecutable}
-	$Result = ${LastExitCode}
-
-	If (${Result} -ne ${ExitSuccess})
-	{
-		Write-Host ${Output} -foreground Red
-	}
-	Write-Host "${TestDescription} (" -nonewline
-
-	If (${Result} -ne ${ExitSuccess})
-	{
-		Write-Host "FAIL" -foreground Red -nonewline
-	}
-	Else
-	{
-		Write-Host "PASS" -foreground Green -nonewline
-	}
-	Write-Host ")"
-
-	Return ${Result}
-}
-
-Function RunTestWithInput
-{
-	param( [string]$TestType )
-
-	$TestDescription = "Testing: ${TestName}"
-	$TestExecutable = "${TestExecutablesDirectory}\una_test_${TestName}.exe"
-
-	If (-Not (Test-Path -Path ${TestExecutable} -PathType Leaf))
-	{
-		Write-Host "${TestDescription} (" -nonewline
-		Write-Host "SKIP" -foreground Cyan -nonewline
-		Write-Host ")"
-
-		Return ${ExitIgnore}
-	}
-	$TestProfileDirectory = "input\.libuna"
-
-	If (-Not (Test-Path -Path ${TestProfileDirectory} -PathType Container))
-	{
-		New-Item -ItemType "directory" -Path ${TestProfileDirectory}
-	}
-	$IgnoreList = ReadIgnoreList ${TestProfileDirectory}
-
-	$Result = ${ExitSuccess}
-
-	ForEach ($TestSetInputDirectory in Get-ChildItem -Path "input" -Exclude ".*")
-	{
-		If (-Not (Test-Path -Path ${TestSetInputDirectory} -PathType Container))
-		{
-			Continue
-		}
-		If (${TestSetInputDirectory} -Contains ${IgnoreList})
-		{
-			Continue
-		}
-		$TestSetName = ${TestSetInputDirectory}.Name
-
-		If (Test-Path -Path "${TestProfileDirectory}\${TestSetName}\files" -PathType Leaf)
-		{
-			$InputFiles = Get-Content -Path "${TestProfileDirectory}\${TestSetName}\files" |
-				Where {$_ -ne ""}
-			$InputFiles = $InputFiles -replace "^","${TestSetInputDirectory}\"
-		}
-		Else
-		{
-			$InputFiles = Get-ChildItem -Path ${TestSetInputDirectory} -Include ${InputGlob}
-		}
-		ForEach ($InputFile in ${InputFiles})
-		{
-			$TestedWithOptions = $False
-
-			ForEach ($OptionSet in ${OptionSets} -split " ")
-			{
-				$InputFileName = ${InputFile}.Name
-				$TestDataOptionFile = "${TestProfileDirectory}\${TestSetName}\${InputFileName}.${OptionSet}"
-
-				If (-Not (Test-Path -Path "${TestDataOptionFile}" -PathType Leaf))
-				{
-					Continue
-				}
-				$OptionsHeader = Get-content -Path "${TestDataOptionFile}" -First 1
-
-				If (-Not (${OptionsHeader} -match "^# libyal test data options"))
-				{
-					Continue
-				}
-				$InputOptions = Get-content -Path "${TestDataOptionFile}" |
-					Select-Object -Skip 1
-
-				$InputOptions = $InputOptions -replace "^offset=","-o"
-				$InputOptions = $InputOptions -replace "^password=","-p"
-				$InputOptions = $InputOptions -replace "^recovery_password=","-r"
-				$InputOptions = $InputOptions -replace "^startup_key=","-s"
-				$InputOptions = $InputOptions -replace "^virtual_address=","-v"
-
-				$Output = Invoke-Expression "${TestExecutable} ${InputOptions} ${InputFile}"
-				$Result = $LastExitCode
-
-				If (${Result} -ne ${ExitSuccess})
-				{
-					Break
-				}
-				$TestedWithOptions = $True
-			}
-			If ((${Result} -eq ${ExitSuccess}) -And (-Not (${TestedWithOptions})))
-			{
-				$Output = Invoke-Expression "${TestExecutable} ${InputFile}"
-				$Result = ${LastExitCode}
-			}
-			If (${Result} -ne ${ExitSuccess})
-			{
-				Break
-			}
-		}
-		If (${Result} -ne ${ExitSuccess})
-		{
-			Break
-		}
-	}
-	If (${Result} -ne ${ExitSuccess})
-	{
-		Write-Host ${Output} -foreground Red
-	}
-	Write-Host "${TestDescription} (" -nonewline
-
-	If (${Result} -ne ${ExitSuccess})
-	{
-		Write-Host "FAIL" -foreground Red -nonewline
-	}
-	Else
-	{
-		Write-Host "PASS" -foreground Green -nonewline
-	}
-	Write-Host ")"
-
-	Return ${Result}
-}
+. .\test_functions.ps1
 
 $TestExecutablesDirectory = GetTestExecutablesDirectory
 
 If (-Not (Test-Path ${TestExecutablesDirectory}))
 {
-	Write-Host "Missing test executables directory." -foreground Red
+	Write-Error "Missing test executables directory"
 
 	Exit ${ExitFailure}
 }
@@ -251,13 +24,15 @@ Foreach (${TestName} in ${LibraryTests} -split " ")
 	{
 		Continue
 	}
-	$Result = RunTest ${TestName}
+	$Result = RunTestBinary ${TestExecutablesDirectory} "una_test_${TestName}"
 
 	If ((${Result} -ne ${ExitSuccess}) -And (${Result} -ne ${ExitIgnore}))
 	{
 		Break
 	}
 }
+
+$TestInputs = GenerateTestInputs "libuna" ${OptionSets}
 
 Foreach (${TestName} in ${LibraryTestsWithInput} -split " ")
 {
@@ -266,13 +41,14 @@ Foreach (${TestName} in ${LibraryTestsWithInput} -split " ")
 	{
 		Continue
 	}
-	If (Test-Path -Path "input" -PathType Container)
+	ForEach ($TestInput in ${TestInputs})
 	{
-		$Result = RunTestWithInput ${TestName}
-	}
-	Else
-	{
-		$Result = RunTest ${TestName}
+		$Result = RunTestBinaryWithInput ${TestExecutablesDirectory} "una_test_${TestName}" ${TestInput}
+
+		If ((${Result} -ne ${ExitSuccess}) -And (${Result} -ne ${ExitIgnore}))
+		{
+			Break
+		}
 	}
 	If ((${Result} -ne ${ExitSuccess}) -And (${Result} -ne ${ExitIgnore}))
 	{
@@ -281,4 +57,3 @@ Foreach (${TestName} in ${LibraryTestsWithInput} -split " ")
 }
 
 Exit ${Result}
-
